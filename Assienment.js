1a2346f67fec47655a0ebf90cfda18d4196f2877
Harsh-Gupta-1111 <hg902756@gmail.com>
2025-05-19 16:32:39 +0530

    Add files via upload

---
+++ Assienment.js
@@ -0,0 +1,5 @@
+function add()
+{
+console.log(document.getElementById("num1").value);
+ //document.getElementById("res").innerHTML= parseInt(document.getElementById("num1").textContent) + parseInt(document.getElementById("num2").textContent);
+}
